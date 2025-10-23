68ce7c3e7f540bafd394360c97f8ab41698f8392
lujia-cmd <lujia.ouyang@student.uts.edu.au>
2025-10-24 09:55:23 +1100

    Update dim_host_month.sql

--- models/gold/star/dim_host_month.sql
+++ models/gold/star/dim_host_month.sql
@@ -1,5 +1,10 @@
-{{ config(materialized='table', post_hook=["analyze {{ this }}"]) }}
-
+{{ config(materialized='table',
+    post_hook=[
+      "create index if not exists {{ this.name }}_range on {{ this }} (host_id, valid_from, valid_to)",
+      "analyze {{ this }}"
+    ]
+) }}
+    
 with snapshot_host as (
     select
     host_id,
@@ -7,24 +12,17 @@ with snapshot_host as (
     host_since,
     host_is_superhost,
     dbt_valid_from::date as valid_from,
-    coalesce(dbt_valid_to, '2021-04-30')::date as valid_to
+    coalesce(dbt_valid_to, '9999-12-31')::date as valid_to
     from {{ ref('host_snapshot') }}
 ),
 
-expanded as (
-    select
-    host_id,
-    host_name,
-    host_since,
-    host_is_superhost,
-    date_trunc('month', gs)::date as year_month
-    from snapshot_host,
-    generate_series(valid_from, valid_to, interval '1 month') as gs
-)
-
 select
-{{ dbt_utils.generate_surrogate_key(['host_id',"to_char(year_month,'YYYY-MM')"]) }} as host_month_id,
-*
-from expanded
+{{ dbt_utils.generate_surrogate_key(['host_id',"to_char(valid_from,'YYYY-MM')"]) }} as host_month_id,
+host_id,
+host_name,
+host_since,
+host_is_superhost,
+valid_from, 
+valid_to
 
-order by host_id, year_month
+from snapshot_host
