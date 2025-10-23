e07673239e8f6c9d61e298c17ded76d5bcee69f8
lujia-cmd <lujia.ouyang@student.uts.edu.au>
2025-10-24 09:27:37 +1100

    Update host_snapshot.sql

--- snapshots/host_snapshot.sql
+++ snapshots/host_snapshot.sql
@@ -19,4 +19,5 @@ host_id, host_name, host_since, host_is_superhost,
 scraped_date::timestamp as scraped_date
 from {{ ref('airbnb_listing') }}
 where host_id is not null
+and source_month = '{{ var("year_month") }}'
 {% endsnapshot %}
