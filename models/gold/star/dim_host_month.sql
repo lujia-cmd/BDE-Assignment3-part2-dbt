cf79b251e67f064ac5fefe1bb7ed57b7b266b4e1
lujia-cmd <lujia.ouyang@student.uts.edu.au>
2025-10-24 13:05:56 +1100

    Update dim_host_month.sql

--- models/gold/star/dim_host_month.sql
+++ models/gold/star/dim_host_month.sql
@@ -12,7 +12,7 @@ with snapshot_host as (
     host_since,
     host_is_superhost,
     date_trunc('month', dbt_valid_from)::date as month_from,
-    (date_trunc('month', coalesce(dbt_valid_to, '9999-12-31'::date))+ interval '1 month')::date as month_to
+    date_trunc('month', dbt_valid_to)::date as month_to
     from {{ ref('host_snapshot') }}
 )
 
@@ -25,3 +25,4 @@ host_is_superhost,
 month_from, 
 month_to
 from snapshot_host
+
