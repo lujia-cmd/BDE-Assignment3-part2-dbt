17a7c23ab483c4a97112397df4d795cf1b494134
lujia-cmd <lujia.ouyang@student.uts.edu.au>
2025-10-25 02:01:50 +1100

    Update host_snapshot.sql

--- snapshots/host_snapshot.sql
+++ snapshots/host_snapshot.sql
@@ -7,17 +7,24 @@
         updated_at='scraped_date::timestamp',
         post_hook=[
             "create index if not exists {{ this.name }}_uk on {{ this }} (host_id)",
-            "create index if not exists {{ this.name }}_vfrom on {{ this }} (dbt_valid_from)",
-            "create index if not exists {{ this.name }}_uk_vf on {{ this }} (host_id, dbt_valid_from)",
             "analyze {{ this }}"
         ]
     )
 }}
+with latest as (
+    select
+    host_id,
+    max(scraped_date::timestamp) as scraped_date
+    from {{ ref('airbnb_listing') }}
+    where host_id is not null
+    group by host_id
+)
 
 select
-host_id, host_name, host_since, host_is_superhost,
+l.host_id, l.host_name, l.host_since, l.host_is_superhost, latest.scraped_date
 scraped_date::timestamp as scraped_date
-from {{ ref('airbnb_listing') }}
-where host_id is not null
+from {{ ref('airbnb_listing') }} l
+join latest using (host_id, scraped_date)
 {% endsnapshot %}
+
 
