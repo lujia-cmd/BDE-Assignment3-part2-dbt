bf05d72bec206ab6db9a58be392dffad4dbe5bba
lujia-cmd <lujia.ouyang@student.uts.edu.au>
2025-10-24 12:21:09 +1100

    Update property_snapshot.sql

--- snapshots/property_snapshot.sql
+++ snapshots/property_snapshot.sql
@@ -24,11 +24,7 @@ with base as (
     scraped_date::timestamp as scraped_date
     from {{ ref('airbnb_listing') }}
     where property_type is not null and room_type is not null and accommodates is not null
-    and source_month = '{{ var("year_month") }}'
 )
 
 select * from base
-
 {% endsnapshot %}
-
-
