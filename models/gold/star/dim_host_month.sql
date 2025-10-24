17e535eeda1db0b80567b58e43ee934a4af0d7b6
lujia-cmd <lujia.ouyang@student.uts.edu.au>
2025-10-24 11:07:21 +1100

    Update dim_host_month.sql

--- models/gold/star/dim_host_month.sql
+++ models/gold/star/dim_host_month.sql
@@ -17,7 +17,7 @@ with snapshot_host as (
 )
 
 select
-{{ dbt_utils.generate_surrogate_key(['host_id',"month_from')"]) }} as host_month_id,
+{{ dbt_utils.generate_surrogate_key(['host_id',"to_char(month_from,'YYYY-MM')"]) }} as host_month_id,
 host_id,
 host_name,
 host_since,
@@ -25,3 +25,4 @@ host_is_superhost,
 month_from, 
 month_to
 from snapshot_host
+
