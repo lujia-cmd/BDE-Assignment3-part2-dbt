97d6881612dd64902cf90adbe2f3ce4194f5f0e0
lujia-cmd <lujia.ouyang@student.uts.edu.au>
2025-10-24 11:06:18 +1100

    Update dim_property_month.sql

--- models/gold/star/dim_property_month.sql
+++ models/gold/star/dim_property_month.sql
@@ -12,32 +12,17 @@ with snapshot_property as (
     property_type,
     room_type,
     accommodates,
-    dbt_valid_from::date as valid_from,
-    coalesce(dbt_valid_to, '9999-12-31')::date as valid_to
+    date_trunc('month', dbt_valid_from)::date as month_from,
+    date_trunc('month', coalesce(dbt_valid_to, '9999-12-31'))::date as month_to
     from {{ ref('property_snapshot') }}
 )
-
-mon as (
-    select
-    property_key, property_type, room_type, accommodates,
-    date_trunc('month', valid_from)::date as month_from,
-    date_trunc('month', valid_to)::date as month_to,
-    row_number() over (
-        partition by property_key, date_trunc('month', valid_from)
-        order by valid_from desc
-    ) as rn
-    from s
-),
-
-del as (select * from m where rn = 1)
     
 select
-{{ dbt_utils.generate_surrogate_key(['property_key', "to_char(month_from, 'YYYY-MM')"]) }} as property_month_id,
+{{ dbt_utils.generate_surrogate_key(['property_key', 'month_from']) }} as property_month_id,
 property_key,
 property_type,
 room_type,
 accommodates,
 month_from,
 month_to
-
-from del
+from snapshot_property
